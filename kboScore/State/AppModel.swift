5fdf57c3684f7ab7164fde7645eb77f032872eb9
whrudals <96468366+rudadlswh@users.noreply.github.com>
2026-05-18 10:51:27 +0900

    refactor: extract game notification transition states

--- kboScore/State/AppModel.swift
+++ kboScore/State/AppModel.swift
@@ -4058,34 +4058,3 @@ private enum ScheduleStartupSyncState {
     case completed
     case failedButDoNotRetryAutomatically
 }
-
-private struct GameCancellationNotificationState: Equatable {
-    let status: GameStatus
-    let isWeatherRelated: Bool
-}
-
-private struct GameContentNotificationState: Equatable {
-    let status: GameStatus
-    let awayScore: Int?
-    let homeScore: Int?
-    let inningText: String?
-    let bases: RunnerState?
-    let balls: Int?
-    let strikes: Int?
-    let outs: Int?
-    let currentBatterName: String?
-    let currentPitcherName: String?
-
-    init(game: GameDetail) {
-        status = game.status
-        awayScore = game.awayScore
-        homeScore = game.homeScore
-        inningText = game.inningText
-        bases = game.bases
-        balls = game.balls
-        strikes = game.strikes
-        outs = game.outs
-        currentBatterName = game.currentBatterName
-        currentPitcherName = game.currentPitcherName
-    }
-}
